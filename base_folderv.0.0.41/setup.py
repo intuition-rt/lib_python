48b4e77811ae42dab656de09b1d7003b347921e6
Marin.cvl <chevaliermarin35@gmail.com>
2024-09-25 09:22:25 +0200

    update
    -> pass to v0.0.41
    -> fix set_led_single

--- base_folderv.0.0.41/setup.py
+++ base_folderv.0.0.41/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-VERSION = '0.0.38'
+VERSION = '0.0.41'
 DESCRIPTION = 'Control ilo robot using python command.'
 with open('README.md') as f:
     long_description = f.read()
