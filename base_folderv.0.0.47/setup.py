3eefd51eca81c8aa00401e3a77ece6250a2f4efd
Marin.cvl <chevaliermarin35@gmail.com>
2025-02-05 15:02:30 +0100

    update v0.046 to 0.047

--- base_folderv.0.0.47/setup.py
+++ base_folderv.0.0.47/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-VERSION = '0.0.46'
+VERSION = '0.0.47'
 DESCRIPTION = 'Control ilo robot using python command.'
 with open('README.md') as f:
     long_description = f.read()
