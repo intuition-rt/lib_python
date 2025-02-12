bf3c71bf0614f5e372d14f15215a230d25411664
Marin.cvl <123023057+MarinCvl@users.noreply.github.com>
2025-02-12 11:17:27 +0100

    Update pen_holder.py
    fix: improve code

--- base_folderv.0.0.48/libname/pen_holder.py
+++ base_folderv.0.0.48/libname/pen_holder.py
@@ -5,12 +5,16 @@ ilo.check_robot_on_wifi()
 
 my_ilo = ilo.robot(1)
 
-my_ilo.drive_single_motor_speed(5,50,20)
+if (my_ilo.ping_single_motor(5)[1] == 1):
+    print("motor 5 connected")
+    my_ilo.drive_single_motor_speed(5,50,20)
     #my_ilo.get_torque_single_motor(5)[1]
-torque = 0
-while (torque<300):  # add time safety (5 seconds)
-    torque =  abs(my_ilo.get_torque_single_motor(5)[1])
-    print(torque)
-    time.sleep(0.2)
-my_ilo.drive_single_motor_speed(5,50,0)
-print("limite value get")
+    torque = 0
+    while (torque<300):  # add time safety (5 seconds)
+        torque =  abs(my_ilo.get_torque_single_motor(5)[1])
+        print(torque)
+        time.sleep(0.2)
+    my_ilo.drive_single_motor_speed(5,50,0)
+    print("limite value get")
+else:
+    print("nothing connected")
