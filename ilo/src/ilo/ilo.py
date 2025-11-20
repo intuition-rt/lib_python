0480043d8d9a9cde7382108f2ff854505c798cb9
Sigmanificient <edhyjox@gmail.com>
2025-11-20 10:25:08 +0100

    Bump version for 0.0.59

--- ilo/src/ilo/ilo.py
+++ ilo/src/ilo/ilo.py
@@ -270,7 +270,7 @@ class _IloUpdater:
         except Exception as e:
             print(f"⚠️ Impossible to check for updates :,(")
 
-_version = "0.0.58"
+_version = "0.0.59"
 
 print("ilo robot library version: ", _version)
 print("For more information about the library use ilo.info() command line")
