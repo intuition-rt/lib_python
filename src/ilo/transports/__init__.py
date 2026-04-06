eacac65605190267759054cdd06bc6ba19db1756
Sigmanificient <edhyjox@gmail.com>
2026-04-06 23:08:45 +0200

    Remove dummy transport

--- src/ilo/transports/__init__.py
+++ src/ilo/transports/__init__.py
@@ -3,12 +3,8 @@ from .ble import BLETransport
 from .serial import SerialTransport
 from .wifi import WiFiTransport
 
-from .dummy import DummyTransport
-
-
 __all__ = (
     "Transport",
-    "DummyTransport",
     "BLETransport",
     "SerialTransport",
     "WiFiTransport"
