f1f7bba78e1fc6cb001b72d6bcacab1fd86bafee
Sigmanificient <edhyjox@gmail.com>
2026-04-07 11:06:42 +0200

    Improve response parser

--- src/ilo/ilo.py
+++ src/ilo/ilo.py
@@ -245,162 +245,168 @@ class Robot:
         """
         if self._debug:
             print(f"[{self._hostname} ~ {self.connection_type}] <-", data)
+
+        trame = data.strip("<>")
+        if trame == "avp0":
+            self._movement_complete.set()
+            return
+        if trame == "ilo":
+            self._response_event.set()
+            return
+        if trame == "92": # set_wifi_credential
+            self._parse_credentials(trame.removeprefix("92"))
+
         try:
-
-            if str(data[1:4]) == "10c":  # get_color_rgb_center
-                self._red_color_center   = int(data[data.find('r')+1: data.find('g')])
-                self._green_color_center = int(data[data.find('g')+1: data.find('b')])
-                self._blue_color_center  = int(data[data.find('b')+1: data.find('>')])
-
-            elif str(data[1:4]) == "10l":  # get_color_rgb_left
-                self._red_color_left   = int(data[data.find('r')+1: data.find('g')])
-                self._green_color_left = int(data[data.find('g')+1: data.find('b')])
-                self._blue_color_left  = int(data[data.find('b')+1: data.find('>')])
-
-            elif str(data[1:4]) == "10d":  # get_color_rgb_right
-                self._red_color_right   = int(data[data.find('r')+1: data.find('g')])
-                self._green_color_right = int(data[data.find('g')+1: data.find('b')])
-                self._blue_color_right  = int(data[data.find('b')+1: data.find('>')])
-
-            elif str(data[1:4]) == "11l":  # get_color_clear
-                self._clear_left = int(data[data.find('l')+1: data.find('m')])
-                self._clear_center = int(data[data.find('m')+1: data.find('r')])
-                self._clear_right = int(data[data.find('r')+1: data.find('>')])
-
-            elif str(data[1:4]) == "12l":  # get_line
-                self._line_left = int(data[data.find('l')+1: data.find('m')])
-                self._line_center = int(data[data.find('m')+1: data.find('r')])
-                self._line_right = int(data[data.find('r')+1: data.find('>')])
-
-            elif str(data[1:4]) == "14t":  # get_line_threshold_value
-                self._line_threshold_value = int(data[data.find('t')+1: data.find('>')])
-
-            elif str(data[1:4]) == "20f":  # get_distance
-                self._distance_front = int(data[data.find('f')+1: data.find('r')])
-                self._distance_right = int(data[data.find('r')+1: data.find('b')])
-                self._distance_back = int(data[data.find('b')+1: data.find('l')])
-                self._distance_left = int(data[data.find('l')+1: data.find('>')])
-
-            elif str(data[1:4]) == "21f":  # get_distance_front
-                self._distance_front = int(data[data.find('f')+1: data.find('>')])
-
-            elif str(data[1:4]) == "22r":  # get_distance_right
-                self._distance_right = int(data[data.find('r')+1: data.find('>')])
-
-            elif str(data[1:4]) == "23b":  # get_distance_back
-                self._distance_back = int(data[data.find('b')+1: data.find('>')])
-
-            elif str(data[1:4]) == "24l":  # get_distance_left
-                self._distance_left = int(data[data.find('l')+1: data.find('>')])
-
-            elif str(data[1:4]) == "30r":  # get_angle - données traités en degrés
-                self._roll = float(data[data.find('r')+1: data.find('p')])
-                self._pitch = float(data[data.find('p')+1: data.find('y')])
-                self._yaw = float(data[data.find('y')+1: data.find('>')])
-
-            elif str(data[1:4]) == "32x":  # get_raw_imu
-                self._accX = float(data[data.find('x')+1: data.find('y')])
-                self._accY = float(data[data.find('y')+1: data.find('z')])
-                self._accZ = float(data[data.find('z')+1: data.find('r')])
-                self._gyroX = float(data[data.find('r')+1: data.find('p')])
-                self._gyroY = float(data[data.find('p')+1: data.find('g')])
-                self._gyroZ = float(data[data.find('g')+1: data.find('>')])
-
-            elif str(data[1:4]) == "40s":  # get_battery
-                self._battery_status = int(data[data.find('s')+1: data.find('p')])
-                self._battery_pourcentage = int(data[data.find('p')+1: data.find('v')])
-                self._battery_voltage = float(data[data.find('v')+1: data.find('>')])
-
-            elif str(data[1:4]) == "50r":  # get_led_color
-                self._red_led = int(data[data.find('r')+1: data.find('g')])
-                self._green_led = int(data[data.find('g')+1: data.find('b')])
-                self._blue_led = int(data[data.find('b')+1: data.find('>')])
-
-            elif str(data[1:4]) == "60i":  # ping_single_motor
-                self._motor_id = int(data[data.find('i')+1: data.find('s')])
-                self._motor_ping = int(data[data.find('s')+1: data.find('>')])
-
-            elif str(data[1:4]) == "611":  # get_single_motor_speed
-                self._motor_id = int(data[data.find('i')+1: data.find('s')])
-                self._motor_speed = int(data[data.find('s')+1: data.find('>')])
-
-            elif str(data[1:4]) == "621":  # get_single_motor_angle
-                self._motor_id = int(data[data.find('i')+1: data.find('s')])
-                self._motor_angle = int(data[data.find('s')+1: data.find('>')])
-
-            elif str(data[1:4]) == "63i":  # get_temp_single_motor
-                self._motor_id = int(data[data.find('i')+1: data.find('s')])
-                self._temp_motor = int(data[data.find('s')+1: data.find('>')])
-
-            elif str(data[1:4]) == "64i":  # get_volt_single_motor
-                self._motor_id = int(data[data.find('i')+1: data.find('s')])
-                self._motor_volt = int(data[data.find('s')+1: data.find('>')])
-
-            elif str(data[1:4]) == "65i":  # get_torque_single_motor
-                self._motor_id = int(data[data.find('i')+1: data.find('s')])
-                self._motor_torque = int(data[data.find('s')+1: data.find('>')])
-
-            elif str(data[1:4]) == "66i":  # get_current_single_motor
-                self._motor_id = int(data[data.find('i')+1: data.find('s')])
-                self._motor_current = int(data[data.find('s')+1: data.find('>')])
-
-            elif str(data[1:4]) == "67i":  # get_motor_is_moving
-                self._motor_id = int(data[data.find('i')+1: data.find('s')])
-                self._motor_is_moving = int(data[data.find('s')+1: data.find('>')])
-
-            elif str(data[1:4]) == "681":  # get_acc_motor
-                self._acc_motor = int(data[data.find('a')+1: data.find('>')])
-
-            elif str(data[1:4]) == "691":  # get_tempo_pos
-                self._tempo_pos = int(data[data.find('t')+1: data.find('>')])
-
-            elif str(data[1:4]) == "71p":  # get_pid
-                self._kp = float(data[data.find('p')+1: data.find('i')])
-                self._ki = float(data[data.find('i')+1: data.find('d')])
-                self._kd = float(data[data.find('d')+1: data.find('>')])
-
-            elif str(data[1:3]) == "92":  # get_wifi_credentials
-
-                self._ssid, self._password = self._parse_credentials(data)
-
-            elif str(data[1:4]) == "93n":  # get_name
-                self._hostname = str(data[data.find('n')+1: data.find('>')])
-
-            elif str(data[1:5]) == "101t":  # get_accessory
-                self._accessory = float(data[data.find('t')+1: data.find('>')])
-
-            elif str(data[1:5]) == "102a":  # get_accessory
-                self._potard_value = float(data[data.find('a')+1: data.find('>')])
-
-            elif str(data[1:4]) == "120":  # get_manufacturing_date
-                self._manufacturing_date = str(data[data.find('s')+1: data.find('>')])
-
-            elif str(data[1:4]) == "130":  # get_first_use_date
-                self._first_use_date = str(data[data.find('s')+1: data.find('>')])
-
-            elif str(data[1:4]) == "140":  # get_product_version
-                self._product_version = str(data[data.find('s')+1: data.find('>')])
-
-            elif str(data[1:4]) == "150":  # get_product_id
-                self._product_id = str(data[data.find('s')+1: data.find('>')])
-
-            elif str(data[1:5]) == "500y": # get_version
-                self._version = str(data[data.find('y')+1: data.find('>')]).removeprefix("v")
-
-            elif str(data[1:4]) == "500":  # get_global_trame
-                self._version = str(data[data.find('y')+1: data.find('>')])
-                print(f"Version: {self._version}")
+            match = re.match(r"^(\d+)(.*)", trame)
+            if not match:
+                return
             
-            elif str(data[1:5]) == "avp0":  # step movement complete
-                self._movement_complete.set()
+            code, payload = match.groups()
+            values = {
+                m[0]: m[1]
+                for m in re.findall(r"([a-z])([-+]?\d*\.?\d*)", payload)
+            }
+
+            try:
+                self._update_state(code, values, trame)
+            except:
+                return
 
             self._response_event.set()
 
         except Exception as e:
-            # -- marin add e to check the error
-            print(f'[COMMUNICATION ERROR] data process: {e}')
-            return None
+            print(f'[COMMUNICATION ERROR] data process: {e} | Trame: {data}')
 
+    def _update_state(self, code: str, v: dict, raw: str):
+        if code == "10":
+            # get_color_rgb_{left,center,right}
+
+            r, g, b = int(v['r']), int(v['g']), int(v['b'])
+
+            if 'c' in raw:
+                self._red_color_center, self._green_color_center, self._blue_color_center = r, g, b
+            elif 'l' in raw:
+                self._red_color_left, self._green_color_left, self._blue_color_left = r, g, b
+            elif 'd' in raw:
+                self._red_color_right, self._green_color_right, self._blue_color_right = r, g, b
+
+        elif code == "11": # get_color_clear
+            self._clear_left = int(v['l'])
+            self._clear_center = int(v['m'])
+            self._clear_right = int(v['r'])
+
+        elif code == "12": # get line
+            self._line_left = int(v['l'])
+            self._line_center = int(v['m'])
+            self._line_right = int(v['r'])
+        elif code == "14": # get_line_threshold_value
+            self._line_threshold_value = int(v['t'])
+
+        # Distance
+
+        elif code == "20": # get_distance
+            self._distance_front = int(v['f'])
+            self._distance_right = int(v['r'])
+            self._distance_back = int(v['b'])
+            self._distance_left = int(v['l'])
+        elif code == "21": # get_distance_front
+            self._distance_front = int(v['f'])
+        elif code == "22": # get_distance_right
+            self._distance_right = int(v['r'])
+        elif code == "23": # get_distance_back
+            self._distance_right = int(v['b'])
+        elif code == "24": # get_distance_left
+            self._distance_right = int(v['l'])
+
+        elif code == "30":  # get_angle
+            self._roll = float(v['r'])
+            self._pitch = float(v['p'])
+            self._yaw = float(v['y'])
+        elif code == "32":  # get_raw_imu
+            self._accX = float(v['x'])
+            self._accY = float(v['y'])
+            self._accZ = float(v['z'])
+            self._gyroX = float(v['r'])
+            self._gyroY = float(v['p'])
+            self._gyroZ = float(v['g'])
+        elif code == "40":  # get_battery
+            self._battery_status = int(v['s'])
+            self._battery_pourcentage = int(v['p'])
+            self._battery_voltage = float(v['v'])
+
+        elif code == "50":  # get_led_color
+            self._red_led = int(v['r'])
+            self._green_led = int(v['g'])
+            self._blue_led = int(v['b'])
+
+        # Motor
+
+        elif code == "60":  # ping_single_motor
+            self._motor_id = int(v['i'])
+            self._motor_ping = int(v['s'])
+
+        elif code == "611":  # get_single_motor_speed
+            self._motor_id = int(v['i'])
+            self._motor_speed = int(v['s'])
+
+        elif code == "621":  # get_single_motor_angle
+            self._motor_id = int(v['i'])
+            self._motor_angle = int(v['s'])
+
+        elif code == "63":  # get_temp_single_motor
+            self._motor_id = int(v['i'])
+            self._temp_motor = int(v['s'])
+
+        elif code == "64":  # get_volt_single_motor
+            self._motor_id = int(v['i'])
+            self._motor_volt = int(v['s'])
+
+        elif code == "65":  # get_torque_single_motor
+            self._motor_id = int(v['i'])
+            self._motor_torque = int(v['s'])
+
+        elif code == "66":  # get_current_single_motor
+            self._motor_id = int(v['i'])
+            self._motor_current = int(v['s'])
+
+        elif code == "67":  # get_motor_is_moving
+            self._motor_id = int(v['i'])
+            self._motor_is_moving = int(v['s'])
+
+        elif code == "681":  # get_acc_motor
+            self._acc_motor = int(v['a'])
+
+        elif code == "691":  # get_tempo_pos
+            self._tempo_pos = int(v['t'])
+
+        elif code == "71":  # get_pid
+            self._kp = float(v['p'])
+            self._ki = float(v['i'])
+            self._kd = float(v['d'])
+
+        elif code == "93":  # get_name
+            self._hostname = str(v['n'])
+
+        elif code == "101t":  # get_accessory
+            self._accessory = float(v['t'])
+
+        elif code == "102":  # get_accessory
+            self._potard_value = float(v['a'])
+
+        elif code == "120":  # get_manufacturing_date
+            self._manufacturing_date = v['s']
+
+        elif code == "130":  # get_first_use_date
+            self._first_use_date = v['s']
+
+        elif code == "140":  # get_product_version
+            self._product_version = v['s']
+
+        elif code == "150":  # get_product_id
+            self._product_id = v['s']
+
+        elif code == "500":  # get_global_trame
+            self._version = v['y']
 
     # -----------------------------------------------------------------------------
     def __del__(self):
@@ -2151,8 +2157,8 @@ class Robot:
             tuple: a tuple containing the ssid and password
         """
         try:
-            inner = data[3:-1]
-            ssid, password = inner.split("{|||}")
+            # TODO: a better approach for this
+            ssid, password = data.split("{|||}")
             ssid = ssid.strip()
             password = password.strip()
             return ssid, password
